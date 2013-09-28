ddc0ac8e4cdec00e2f78abfe11ec6eac977c3a46
Giorgio <giorgio.pellero@gmail.com>
2013-09-28 16:01:03 +0200

    Fixed stuff
    yep

--- SkypeBot/ECommand.cs
+++ SkypeBot/ECommand.cs
@@ -1,17 +1,20 @@
-public enum ECommand : string
+namespace SkypeBot
 {
-  GO_OFFLINE = "go offline",
-  HELLO = "hello",
-  HELP = "help",
-  DATE = "date",
-  TIME = "time",
-  WHO = "who",
-  WHO_AM_I = "who am i?",
-  PENIS = "penis",
-  YOUR_MOTHER = "your mother",
-  HI = "hi",
-  WAKE_HIM_UP = "wake him up!",
-  CONTATCS_AMOUNT = "contacts amount",
-  IGNORE_ME = "ignore me",
-  UNIGNORE_ME = "unignore me"
+    public enum ECommand : string
+    {
+        GO_OFFLINE = "go offline",
+        HELLO = "hello",
+        HELP = "help",
+        DATE = "date",
+        TIME = "time",
+        WHO = "who",
+        WHO_AM_I = "who am i?",
+        PENIS = "penis",
+        YOUR_MOTHER = "your mother",
+        HI = "hi",
+        WAKE_HIM_UP = "wake him up!",
+        CONTATCS_AMOUNT = "contacts amount",
+        IGNORE_ME = "ignore me",
+        UNIGNORE_ME = "unignore me"
+    }
 }
