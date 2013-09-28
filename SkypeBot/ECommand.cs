3723d4b5be827f3b4603a058982dba37f1925aeb
Giorgio <giorgio.pellero@gmail.com>
2013-09-28 15:58:18 +0200

    Added commands enum
    For extensibility.

---
+++ SkypeBot/ECommand.cs
@@ -0,0 +1,17 @@
+public enum ECommand : string
+{
+  GO_OFFLINE = "go offline",
+  HELLO = "hello",
+  HELP = "help",
+  DATE = "date",
+  TIME = "time",
+  WHO = "who",
+  WHO_AM_I = "who am i?",
+  PENIS = "penis",
+  YOUR_MOTHER = "your mother",
+  HI = "hi",
+  WAKE_HIM_UP = "wake him up!",
+  CONTATCS_AMOUNT = "contacts amount",
+  IGNORE_ME = "ignore me",
+  UNIGNORE_ME = "unignore me"
+}
