f4f87dfd9cc4052a99bc3a6522626399f5818521
Fabian Fischer <speisewagen96@gmx.de>
2013-09-28 23:28:17 +0200

    Removing shit

--- SkypeBot/ECommand.cs
+++ SkypeBot/ECommand.cs
@@ -67,19 +67,5 @@ namespace SkypeBot
     
             return output;
         }
-
-        public static string GetStringValue(string value)
-        {
-            string output = null;
-            Type type = value.GetType();
-            FieldInfo fi = type.GetField(value.ToString());
-            StringValue[] attrs = fi.GetCustomAttributes(typeof(StringValue), false) as StringValue[];
-            if (attrs.Length > 0)
-            {
-                output = attrs[0].Value;
-            }
-
-            return output;
-        }
     }
 }
